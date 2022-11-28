0db2050c8a0bb62dd4757a06bb5bdb726cbf2e94
Omar Macias <a01635795@tec.mx>
2022-11-28 00:23:29 -0600

    Index almost finished, table missing

--- WePark/WePark/settings.py
+++ WePark/WePark/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'WePark.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': { # Connected to a SQL Server database in Google Cloud
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
